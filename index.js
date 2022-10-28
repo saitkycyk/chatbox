c14e992e5cd2968d1eddb3e9bc061485be49ddfa
Sait Kycyk <kys@sml.at>
2022-10-28 11:15:06 +0200

    autofind receiver via localip

--- index.js
+++ index.js
@@ -4,23 +4,10 @@ const app = express()
 app.use(express.json())
 const net = require('net');
 const axios = require('axios');
-const host = process.env.host
+var host = process.env.host ?? localhost
 const port = process.env.port
 const target = `http://${host}:${port}`;
 var receiverStatus = null;
-
-setInterval(function(){
-    checkReceiver()
-}, 1000);
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-readline.on('line', (input) => {
-    sendMessage(input);
-});
 
 app.post('/', (req, res) => {
     console.log('\x1b[32m', "> "+req.body.message, '\x1b[0m');
@@ -28,19 +15,33 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Chatbox started, listening to port: ${port}...`)
 })
+
+findIp().then(
+    setInterval(function() {
+        checkReceiver();
+    }, 1000)
+);
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.on('line', (input) => {
+    sendMessage(input);
+});
 
 function sendMessage(message) {
 
     var message = `${getDate()}: ${message}`
 
     axios.post(target, {message}, {timeout: 3000})
-      .then(function (response) {
-      })
-      .catch(function (error) {
-        console.log('\x1b[31m', "> Message not sent!", '\x1b[0m');
-      });
+        .then(function (response) {})
+        .catch(function (error) {
+            console.log('\x1b[31m', "> Message not sent!", '\x1b[0m');
+        });
 }
 
 function getDate() {
@@ -53,18 +54,74 @@ function checkReceiver() {
     sock.on('connect', function() {
         if("online" !== receiverStatus) {
             console.log('\x1b[32m',`Receiver is online!`, '\x1b[0m')
+            receiverStatus = "online";
         }
-        receiverStatus = "online";
         sock.destroy();
     }).on('error', function(e) {
         if("offline" !== receiverStatus) {
             console.log('\x1b[31m' ,`Receiver is offline!`, '\x1b[0m')
+            receiverStatus = "offline";
         }
-        receiverStatus = "offline";
     }).on('timeout', function(e) {
         if("offline" !== receiverStatus) {
             console.log('\x1b[31m', `Receiver is offline!`, '\x1b[0m')
+            receiverStatus = "offline";
         }
-        receiverStatus = "offline";
     }).connect(port, host);
+}
+
+
+function findIp() {
+    return new Promise((resolve) => {
+        console.log("Trying to find the receiver...");
+    
+        findMyIp().then(function(myip) {
+            if(myip == "0.0.0.0") {
+                console.log("Could not find the local IP! IP autodetect failed!")
+                return false;
+            }
+
+            const seperatedIp = myip.split('.');
+            for(var i = 0; i < 255; i++) {
+                if(i == seperatedIp[3]) {continue;}
+
+                findReceiver(`${seperatedIp[0]}.${seperatedIp[1]}.${seperatedIp[2]}.${i}`).then(function(receiverIp) {
+                    if(receiverIp) {
+                        host = receiverIp
+                        resolve(true);
+                    }
+                });
+            }
+        });
+    })
+}
+
+function findReceiver(possibleIp) {
+    return new Promise((resolve) => {
+        var sock = new net.Socket();
+        sock.setTimeout(500);
+        sock.on('connect', function() {
+            resolve(possibleIp);
+        }).on('error', function(e) {
+            resolve(false);
+        }).on('timeout', function(e) {
+            resolve(false);
+        }).connect(port, possibleIp);
+    });
+}
+
+function findMyIp() {
+    return new Promise((resolve) => {
+        var interfaces = require('os').networkInterfaces();
+        for (var devName in interfaces) {
+        var iface = interfaces[devName];
+    
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+            resolve(alias.address);
+        }
+        }
+        resolve('0.0.0.0');
+    });
 }
